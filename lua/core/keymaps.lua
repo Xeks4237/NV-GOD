0ea81aab40b976a8a9e6a144ec7d76a4942af453
Xeks4237 <amid.muradov111@gmail.com>
2025-09-07 21:19:35 +0400

    minor changes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,49 +1,68 @@
--- Setups <leader> to space
+-- This file contains main keymaps of NV-GOD
+-- Most of the file is inspired by keymaps of
+-- LazyVim and personal keymaps of my Friend
+
+-- P.S If you are new in NVim please write :Tutor command and after completing simple tutorial, return here
+
+-- In NVim some keys are writed shortly like for example:
+-- "CTRL + a" is a <C-a>
+-- "SHIFT + a" is a <S-a>
+-- "ALT + a" is a <A-a>
+
+-- <leader> key is a main key which is used in most of the keymaps for start
+-- In this case <leader> is a space key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- makes x key not copy symbhol after deleting it
+-- Makes x key don't copy symbhol under a cursor after deleting it
+-- Someone likes it, someone not, just choose if you want it
 vim.keymap.set("n", "x", '"_x' )
 
--- Keymaps to Resize Splits by holding CTRL and using arrow keys
+-- Keymaps for creating splits of window
+vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split window horizontally", remap = true })
+vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split window vertically", remap = true })
+vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Close window splith", remap = true })
+
+-- Keymaps to resize splits of window
+-- by holding CTRL and pressing arrow keys
 vim.keymap.set("n", "<C-Up>", ":resize -2<CR>")
 vim.keymap.set("n", "<C-Down>", ":resize +2<CR>")
 vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>")
 vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>")
 
--- Navigating beetween splits of windows
+-- Navigating beetween splits of window
+-- by holding CTRL and pressing hjkl (like ←↓↑→)
 vim.keymap.set("n", '<C-k>', ':wincmd k<CR>')
-vim.keymap.set("n", '<C-j>', ':wincmd j<CR>', sioent)
+vim.keymap.set("n", '<C-j>', ':wincmd j<CR>')
 vim.keymap.set("n", '<C-h>', ':wincmd h<CR>')
 vim.keymap.set("n", '<C-l>', ':wincmd l<CR>')
 
--- For creating splits of window
-vim.keymap.set("n", "<leader>-", "<C-W>s", { desc = "Split Window Below", remap = true })
-vim.keymap.set("n", "<leader>|", "<C-W>v", { desc = "Split Window Right", remap = true })
-vim.keymap.set("n", "<leader>wd", "<C-W>c", { desc = "Delete Window", remap = true })
-
 -- Tab Managment
-vim.keymap.set("n", "<leader><tab>n", "<cmd>tabnew<cr>", { desc = "New Tab" })
-vim.keymap.set("n", "<leader><tab>]", "<cmd>tabnext<cr>", { desc = "Next Tab" })
-vim.keymap.set("n", "<leader><tab>[", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
-vim.keymap.set("n", "<leader><tab>w", "<cmd>tabclose<cr>", { desc = "Close Tab" })
+vim.keymap.set("n", "<leader>tn", "<cmd>tabnew<cr>", { desc = "New Tab" })
+vim.keymap.set("n", "<C-Tab>", "<cmd>tabnext<cr>", { desc = "Next Tab" })
+vim.keymap.set("n", "<C-S-Tab>", "<cmd>tabprevious<cr>", { desc = "Previous Tab" })
+vim.keymap.set("n", "<leader>tw", "<cmd>tabclose<cr>", { desc = "Close Tab" })
 
--- Open NetRw Directory Listing (BuiltIn Explorer)
+-- Open NetRw Directory Listing (BuiltIn Explorer of NVim)
 vim.keymap.set("n", "<C-e>", ":Ex<CR>")
 
--- Save file
-vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+-- Save file with CTRL + s
+vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><ESC>", { desc = "Save File" })
 
--- Quit from file
-vim.keymap.set("n", "<C-q>", ":q<CR>")
+-- Just quit everything
+-- gonna give error if you have not saved changes
+vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>", { desc = "Quit NVim" })
 
--- Just Quit
-vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+-- Force quit everything
+-- gonna ignore not saved changes, Can make lost of data!
+vim.keymap.set("n", "<leader><C-q>", "<cmd>qa!<CR>", { desc = "FORCE quit NVim" })
 
 -- Open lazy.nvim package manager
 vim.keymap.set("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Lazy" })
 
--- Move Lines
+-- Keymaps to move lines of code by using:
+-- ALT + j or k on the line with cursor or
+-- on the lines of code chosen on visual mode
 vim.keymap.set("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
 vim.keymap.set("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
 vim.keymap.set("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
@@ -52,6 +71,8 @@ vim.keymap.set("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=g
 vim.keymap.set("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
 
 -- Buffer Managment
+-- I recomment you to read some documentation on what is that
+-- Because description gonna take to much lines :/
 vim.keymap.set("n", "<S-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
 vim.keymap.set("n", "[b", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
@@ -60,14 +81,16 @@ vim.keymap.set("n", "<leader>bb", "<cmd>e #<cr>", { desc = "Switch to Other Buff
 vim.keymap.set("n", "<leader>`", "<cmd>e #<cr>", { desc = "Switch to Other Buffer" })
 vim.keymap.set("n", "<leader>bD", "<cmd>:bd<cr>", { desc = "Delete Buffer and Window" })
 
--- Clear search, diff update and redraw
+-- Keymap to clear search, diff update and redraw easyly
 vim.keymap.set(
     "n",
-    "<leader>ur",
+    "<leader>fr",
     "<Cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>",
     { desc = "Redraw / Clear hlsearch / Diff Update" }
 )
 
+-- Keymaps for easier navigation on search results on file
+-- Read more about this thing on:
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 vim.keymap.set("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
 vim.keymap.set("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
@@ -77,25 +100,26 @@ vim.keymap.set("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Se
 vim.keymap.set("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
 
 -- Add undo break-points
+-- Makes some symbhols be a points where undo takes break
 vim.keymap.set("i", ",", ",<c-g>u")
 vim.keymap.set("i", ".", ".<c-g>u")
 vim.keymap.set("i", ";", ";<c-g>u")
 
--- Keywordprg
+-- Ability to search in man word under a cursor
 vim.keymap.set("n", "<leader>K", "<cmd>norm! K<cr>", { desc = "Keywordprg" })
 
--- Better indenting
+-- Makes indenting in visual mode more "consistent"
 vim.keymap.set("v", "<", "<gv")
 vim.keymap.set("v", ">", ">gv")
 
--- Commenting
+-- Keymaps for better commenting lines like this one
 vim.keymap.set("n", "gco", "o<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Below" })
 vim.keymap.set("n", "gcO", "O<esc>Vcx<esc><cmd>normal gcc<cr>fxa<bs>", { desc = "Add Comment Above" })
 
--- New file
-vim.keymap.set("n", "<leader>fn", "<cmd>enew<cr>", { desc = "New File" })
+-- Keymap to make a new file, by using CTRL + n
+vim.keymap.set("n", "<C-n>", "<cmd>enew<cr>", { desc = "New File" })
 
--- Location list
+-- Location list is a location list 🗿
 vim.keymap.set("n", "<leader>xl", function()
   local success, err = pcall(vim.fn.getloclist(0, { winid = 0 }).winid ~= 0 and vim.cmd.lclose or vim.cmd.lopen)
   if not success and err then
@@ -103,7 +127,7 @@ vim.keymap.set("n", "<leader>xl", function()
   end
 end, { desc = "Location List" })
 
--- Quickfix list
+-- Quickfix list is a quickfix list 🗿
 vim.keymap.set("n", "<leader>xq", function()
   local success, err = pcall(vim.fn.getqflist({ winid = 0 }).winid ~= 0 and vim.cmd.cclose or vim.cmd.copen)
   if not success and err then
@@ -114,32 +138,18 @@ end, { desc = "Quickfix List" })
 vim.keymap.set("n", "[q", vim.cmd.cprev, { desc = "Previous Quickfix" })
 vim.keymap.set("n", "]q", vim.cmd.cnext, { desc = "Next Quickfix" })
 
--- diagnostic
-local diagnostic_goto = function(next, severity)
-  local go = next and vim.diagnostic.goto_next or vim.diagnostic.goto_prev
-  severity = severity and vim.diagnostic.severity[severity] or nil
-  return function()
-    go({ severity = severity })
-  end
-end
-vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
-vim.keymap.set("n", "]d", diagnostic_goto(true), { desc = "Next Diagnostic" })
-vim.keymap.set("n", "[d", diagnostic_goto(false), { desc = "Prev Diagnostic" })
-vim.keymap.set("n", "]e", diagnostic_goto(true, "ERROR"), { desc = "Next Error" })
-vim.keymap.set("n", "[e", diagnostic_goto(false, "ERROR"), { desc = "Prev Error" })
-vim.keymap.set("n", "]w", diagnostic_goto(true, "WARN"), { desc = "Next Warning" })
-vim.keymap.set("n", "[w", diagnostic_goto(false, "WARN"), { desc = "Prev Warning" })
-
--- Highlights under cursor
+-- Inspecting under a cursor
 vim.keymap.set("n", "<leader>ui", vim.show_pos, { desc = "Inspect Pos" })
 vim.keymap.set("n", "<leader>uI", function() vim.treesitter.inspect_tree() vim.api.nvim_input("I") end, { desc = "Inspect Tree" })
 
--- Terminal Mappings
+-- Keymap to hide a Terminal
 vim.keymap.set("t", "<C-/>", "<cmd>close<cr>", { desc = "Hide Terminal" })
 vim.keymap.set("t", "<c-_>", "<cmd>close<cr>", { desc = "which_key_ignore" })
 
 
--- native snippets. only needed on < 0.11, as 0.11 creates these by default
+-- Native snippets. only needed on < 0.11, as 0.11 creates these by default
+-- If you are in correct version which doesn't needs that,
+-- Then you just can remove this part of a code
 if vim.fn.has("nvim-0.11") == 0 then
   vim.keymap.set("s", "<Tab>", function()
     return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"

7ce1b03bb23a765e03971fe2b3492f0ef235140b
Xeks4237 <amid.muradov111@gmail.com>
2025-09-07 16:40:38 +0400

    minor changes

--- lua/lazy/plugins.lua
+++ lua/lazy/plugins.lua
@@ -1,24 +1,35 @@
+-- This file contains a list of all plugins which
+-- lazy.nvim package manager gonna load (lua/lazy/lazy.lua)
+-- P.S it's not necessary to give a names to all plugins
+-- but some plugins/colorschemes requires it (like catppuccin theme)
 return {
     {
-      "folke/which-key.nvim",
-      name = "which-key",
-      opts = function()
-        return require("configs.plugins.which-key")
-      end,
+        "folke/which-key.nvim",
+        name = "which-key",
+        opts = function()
+            return require("configs.plugins.which-key")
+        end,
     },
     {
-      "nvim-telescope/telescope.nvim",
-      name = "telescope",
-      opts = function()
-          return require("configs.plugins.telescope")
-      end,
+        "nvim-telescope/telescope.nvim",
+        name = "telescope",
+        opts = function()
+            return require("configs.plugins.telescope")
+        end,
     },
     {
-      "nvim-tree/nvim-web-devicons",
-      name = "nvim-web-devicons",
-      opts = function()
-          return require("configs.plugins.nvim-web-devicons")
-      end,
+        "nvim-tree/nvim-web-devicons",
+        name = "nvim-web-devicons",
+        opts = function()
+            return require("configs.plugins.nvim-web-devicons")
+        end,
+    },
+    {
+        "mason-org/mason.nvim",
+        name = "mason",
+        opts = function()
+            return require("configs.plugins.mason")
+        end,
     },
 }
 

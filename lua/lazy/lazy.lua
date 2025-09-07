41cbfc1bc7df8f163e0af8fba2dd7dfe7b5b0f96
Xeks4237 <amid.muradov111@gmail.com>
2025-09-07 13:25:01 +0400

    Changed where lazy-lock.json file is located and added more comments

--- lua/lazy/lazy.lua
+++ lua/lazy/lazy.lua
@@ -1,3 +1,7 @@
+-- This is a main file which
+-- BootStraps lazy.nvim package manager
+-- from https://github.com/folke/lazy.nvim
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -14,13 +18,29 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Settings for lazy.nvim package manager
 require("lazy").setup({
+    -- Changes location of lazy-lock.json from
+    -- default ~/.config/nvim/lazy-lock.json
+    -- to ~/.config/nvim/lua/lazy/lazy-lock.json
+    lockfile = "~/.config/nvim/lua/lazy/lazy-lock.json",
 	spec = {
+        -- Choses a files/directorys in lua directory where
+        -- lazy.nvim package manager loads files
+        -- P.S Seperating plugins and colorschemes needed
+        -- only for "consistency"
 		{ import = "lazy.plugins" },
         { import = "lazy.colorschemes" },
+        -- P.S olso it can have a table with paths to seperate files
+        -- for each plugin/colorscheme seperatly (but it makes this file to big and blooated)
 	},
 	checker = {
+        -- Checks changes on files which lazy.mvim package manager uses to load plugins
+        -- if "enabled = false" plugin changes needs restart of nvim to be updated
 		enabled = true,
+        -- if "notify = true" than it makes a notification
+        -- every time when detects a changes in files
+        -- (Isn't usefull in most of the cases)
 		notify = false,
 	},
 })

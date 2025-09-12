ee3c1426da204be3d09b506d01dcb6a74a521039
Xeks4237 <amid.muradov111@gmail.com>
2025-09-12 14:50:45 +0400

    Minor Changes to greeting screen, added tmp logo of NV-GOD

--- lua/configs/plugins/alpha-nvim.lua
+++ lua/configs/plugins/alpha-nvim.lua
@@ -1,28 +1,26 @@
--- This file is loadedd using init.lua instead of plugin.lua
+-- This file is loaded using init.lua instead of plugin.lua
 -- Because it's more easy and convenient to do
--- P.S I used a theme from 
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
 -- Set header/logo which we need
 dashboard.section.header.val = {
-    "                                                     ",
-    "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-    "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-    "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-    "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-    "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-    "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-    "                        btw                          ",
+    "    ███╗   ██╗██╗   ██╗       ██████╗  ██████╗ ██████╗   ",
+    "    ████╗  ██║██║   ██║      ██╔════╝ ██╔═══██╗██╔══██╗  ",
+    "    ██╔██╗ ██║██║   ██║█████╗██║  ███╗██║   ██║██║  ██║  ",
+    "    ██║╚██╗██║╚██╗ ██╔╝╚════╝██║   ██║██║   ██║██║  ██║  ",
+    "    ██║ ╚████║ ╚████╔╝       ╚██████╔╝╚██████╔╝██████╔╝  ",
+    "    ╚═╝  ╚═══╝  ╚═══╝         ╚═════╝  ╚═════╝ ╚═════╝   ",
+    "    not final logo                              v0.0.1   ",
 }
 
 -- Sets buttons which gonna be shown below the logo with their icons
 dashboard.section.buttons.val = {
             dashboard.button("n", "  New File", "<cmd>ene | startinsert<CR>"),
-            dashboard.button("r", "  Recent Files", "<cmd>Telescope oldfiles<CR>"),
             dashboard.button("f", "  Find File", "<cmd>Telescope find_files<CR>"),
             dashboard.button("g", "  Find Text", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("r", "  Recent Files", "<cmd>Telescope oldfiles<CR>"),
             dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<CR>"),
-            dashboard.button("m", "  Mason", "<cmd>Mason<CR>"),
+            dashboard.button("m", "  Mason", "<cmd>Mason<CR>"),
             dashboard.button("c", "  Config", "<cmd>Telescope find_files cwd=~/.config/nvim<CR>"),
             dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
 }
@@ -35,3 +33,5 @@ vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
 ]])
 
+-- P.S I gonna make this able to load .png files instead of ASCII arts in future using image.nvim plugin
+-- To give you ability to set a anime girl as a logo:3

0162e66fbf6e786617c3fac0c38727188ab3fea5
Xeks4237 <amid.muradov111@gmail.com>
2025-09-12 14:54:50 +0400

    Minor Changes

--- lua/lazy/plugins.lua
+++ lua/lazy/plugins.lua
@@ -93,16 +93,10 @@ return {
         priority = 100,
         lazy = false,
         event = "VimEnter",
+        -- Settings for this plugins is loaded in init.lua
         -- opts = function ()
         --     return require("configs.plugins.alpha-nvim")
         -- end
-    },
-    {
-        "3rd/image.nvim",
-        build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
-        opts = {
-            processor = "magick_cli",
-        } 
     },
 }
 

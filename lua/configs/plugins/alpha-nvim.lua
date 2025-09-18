731f4a16f63f7b4a602c2dc4a0d44f1b76f56731
Xeks4237 <amid.muradov111@gmail.com>
2025-09-18 20:02:36 +0400

    Added comments to alpha-nvim.lua

--- lua/configs/plugins/alpha-nvim.lua
+++ lua/configs/plugins/alpha-nvim.lua
@@ -17,15 +17,16 @@ dashboard.section.header.val = {
 
 -- Sets buttons which gonna be shown below the logo with their icons
 dashboard.section.buttons.val = {
-    dashboard.button("n", "  New File", "<cmd>ene | startinsert<CR>"),
-    dashboard.button("f", "  Find File", "<cmd>Telescope find_files<CR>"),
-    dashboard.button("g", "  Find Text", "<cmd>Telescope live_grep<CR>"),
-    dashboard.button("o", "  Recent Files", "<cmd>Telescope oldfiles<CR>"),
-    dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<CR>"),
-    dashboard.button("m", "  Mason", "<cmd>Mason<CR>"),
-    dashboard.button("?", "  Keymaps", "<cmd>lua require('which-key').show('', { mode = 'n' })<CR>"),
-    dashboard.button("c", "  Config", "<cmd>Telescope find_files cwd=~/.config/nvim<CR>"),
-    dashboard.button("q", "  Quit", "<cmd>qa<CR>"),
+    dashboard.button("n", "  New File", "<cmd>ene | startinsert<CR>"), -- Opens a new file
+    dashboard.button("f", "  Find File", "<cmd>Telescope find_files<CR>"), -- Allows to search files in directory where you opened nvim
+    dashboard.button("g", "  Find Text", "<cmd>Telescope live_grep<CR>"), -- Allows to Search text in files using live_grep of telescope plugin
+    dashboard.button("o", "  Recent Files", "<cmd>Telescope oldfiles<CR>"), -- Allows to Search files which are were opened before using telescope plugin
+    dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<CR>"), -- Opens ui of lazy.nvim package maneger
+    dashboard.button("m", "  Mason", "<cmd>Mason<CR>"), -- Opens ui of mason.nvim
+    -- dashboard.button("q", " Get Help", ""), -- Future documentation gonna go here
+    dashboard.button("?", "  Keymaps", "<cmd>lua require('which-key').show('', { mode = 'n' })<CR>"), -- Shows popup of which-key plugin with most the keymaps
+    dashboard.button("c", "  Config", "<cmd>Telescope find_files cwd=~/.config/nvim<CR>"), -- Allows to Search files in directory with nvim configs
+    dashboard.button("q", "  Quit", "<cmd>qa<CR>"), -- Quits nvim
 }
 
 -- Send config to alpha
@@ -36,5 +37,6 @@ vim.cmd([[
     autocmd FileType alpha setlocal nofoldenable
 ]])
 
--- P.S I gonna make this able to load .png files instead of ASCII arts in future using image.nvim plugin
+-- P.S I gonna make this able to load .png files instead of ASCII arts
+-- In the future using image.nvim plugin
 -- To give you ability to set a anime girl as a logo:3

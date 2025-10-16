9dff1362b1a58ef74299fde8517dbf756756f1e4
Xeks4237 <amid.muradov111@gmail.com>
2025-10-16 22:26:43 +0400

    Added harpoon

---
+++ lua/nvgod/plugins/harpoon.lua
@@ -0,0 +1,11 @@
+return {
+    "ThePrimeagen/harpoon", branch = "harpoon2",
+    name = "harpoon",
+    lazy = true,
+    opts = {
+        settings = {
+            save_on_toggle = true, -- Save marks when toggling the quick menu
+            sync_on_ui_close = true, -- Sync marks when closing the UI
+        },
+    },
+}

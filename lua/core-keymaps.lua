60586302a0c611f2226a34475a65b572e21039ef
Xeks4237 <amid.muradov111@gmail.com>
2026-03-23 14:43:01 +0400

    Minor Changes to core-keymaps.lua

--- lua/core-keymaps.lua
+++ lua/core-keymaps.lua
@@ -10,23 +10,14 @@ vim.g.maplocalleader = ' '
 -- Someone likes it, someone not, just choose if you want it
 vim.keymap.set('n', 'x', '"_x', { desc = 'Delete symbhol under the cursor' })
 
--- Keymaps to resize splits of window
--- by holding CTRL and pressing arrow keys
-vim.keymap.set('n', '<C-w><C-Up>', '<cmd>resize -2<CR>', { desc = 'Vertical Resize Up' })
-vim.keymap.set('n', '<C-w><C-Down>', '<cmd>resize +2<CR>', { desc = 'Vertical Resize Down' })
-vim.keymap.set('n', '<C-w><C-Left>', '<cmd>vertical resize -2<CR>', { desc = 'Horizontal Resize' })
-vim.keymap.set('n', '<C-w><C-Right>', '<cmd>vertical resize +2<CR>', { desc = 'Horizontal Resize' })
-
 -- Save file/buffer using CTRL + s (same as using :w command)
-vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<CR><ESC>', { desc = 'Save File' })
+vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<CR><ESC>', { desc = 'Write Changes' })
 
--- Just quit everything
--- gonna give error if you have not saved changes
+-- Keymap for doing quitting stuff
+vim.keymap.set('n', 'q', '<cmd>qa<CR>', { desc = 'Quit NeoVim' })
+
+-- Quit everything
 vim.keymap.set('n', '<C-q>', '<cmd>qa<CR>', { desc = 'Quit NeoVim' })
-
--- Force Quit everything
--- WARN: Gonna ignore not saved changes, Can make lost of data!
-vim.keymap.set('n', '<C-S-q>', '<cmd>qa!<CR>', { desc = 'FORCE Quit' })
 
 -- Keymaps to move selected lines of code by using ALT + j OR k
 -- In Normal mode:
@@ -39,7 +30,7 @@ vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>==gi', { desc = 'Move Up', silent =
 vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { desc = 'Move Down', silent = true })
 vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move Up', silent = true })
 
--- Clear highlights of search when pressing <Esc> in normal mode
+-- Clear highlights of search by pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Keymap to clear Search Highlight, CommamdLine, and also Redraws UI
@@ -61,23 +52,12 @@ vim.keymap.set('x', 'N', "v:searchforward ? 'N' : 'n'", { expr = true, desc = 'P
 vim.keymap.set('o', 'n', "v:searchforward ? 'n' : 'N'", { expr = true, desc = 'Next Search Result' })
 vim.keymap.set('o', 'N', "v:searchforward ? 'N' : 'n'", { expr = true, desc = 'Previous Search Result' })
 
--- Add undo break-points
--- Makes some symbols be a points where undo(u key) takes break
-vim.keymap.set('i', ',', ',<c-g>u')
-vim.keymap.set('i', '.', '.<c-g>u')
-vim.keymap.set('i', ';', ';<c-g>u')
-
 -- Makes indenting in visual mode easier
 vim.keymap.set('v', '<', '<gv', { desc = 'Indent left' })
 vim.keymap.set('v', '>', '>gv', { desc = 'Indent right' })
 
--- Keymap to hide a Terminal
-vim.keymap.set('t', '<C-/>', '<cmd>close<CR>', { desc = 'Hide Terminal' })
-vim.keymap.set('t', '<c-_>', '<cmd>close<CR>', { desc = 'which_key_ignore' })
-
--- Keymap to open quickfix list
-vim.keymap.set('n', '<leader>dq', '<cmd>copen><CR>', { desc = 'Open QuickFix List' })
-
 -- Some keymaps to fix behavior of "Home" and "End" keys while being in tmux window/pane
-vim.keymap.set({ 'n', 'v', 'i' }, '<Find>', '0')
-vim.keymap.set({ 'n', 'v', 'i' }, '<Select>', '$')
+vim.keymap.set({ 'n', 'v' }, '<Find>', '0')
+vim.keymap.set({ 'n', 'v' }, '<Select>', '$')
+vim.keymap.set({ 'i' }, '<Find>', '')
+vim.keymap.set({ 'i' }, '<Select>', '')

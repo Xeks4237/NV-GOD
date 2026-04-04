6e25763af0a24237fa2143ade9877b6d0f1bb39a
Xeks4237 <amid.muradov111@gmail.com>
2026-04-04 10:50:01 +0400

    Removed some unneded keysmaps

--- lua/core-keymaps.lua
+++ lua/core-keymaps.lua
@@ -1,20 +1,14 @@
 -- NOTE: This file contains main keymaps of NV-GOD which are NOT related to plugins
 -- Keymaps for plugins are writed in "extra-keymaps.lua" file
 
--- <leader> key is a main starting key for NeoVim keybindings
--- In this case <leader> is a Space
+-- <leader> key is a main starting key for your custom NeoVim keybindings
+-- In this case <leader> is Space key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Makes x key don't copy symbhol under a cursor after deleting it
 -- Someone likes it, someone not, just choose if you want it
 vim.keymap.set('n', 'x', '"_x', { desc = 'Delete symbhol under the cursor' })
-
--- Save file/buffer using CTRL + s (same as using :w command)
-vim.keymap.set({ 'i', 'x', 'n', 's' }, '<C-s>', '<cmd>w<CR><ESC>', { desc = 'Write Changes' })
-
--- Keymap for doing quitting stuff
-vim.keymap.set('n', 'q', '<cmd>q<CR>', { desc = 'Quit' })
 
 -- Keymaps to move selected lines of code by using ALT + j OR k
 -- In Normal mode:
@@ -30,28 +24,14 @@ vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { desc = 'Move Up', silent = tr
 -- Clear highlights of search by pressing <Esc> in normal mode
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
--- Keymap to clear Search Highlight, CommamdLine, and also Redraws UI
-vim.keymap.set(
-	'n',
-	'<C-f>r',
-	'<cmd>nohlsearch<Bar>diffupdate<Bar>normal! <C-L><CR>',
-	{ desc = 'Redraw / Clear hlsearch / Diff Update' }
-)
-
--- Keymaps for easier navigation in search results
--- Makes n key to move your cursor to the next search result
--- Makes N key to move your cursor to the previous search result
--- Read more about it: https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
+-- Makes n key to always move your cursor to the next search result
+-- Makes N key to always move your cursor to the previous search result
 vim.keymap.set('n', 'n', "v:searchforward ? 'n' : 'N'", { expr = true, desc = 'Next Search Result' })
 vim.keymap.set('n', 'N', "v:searchforward ? 'N' : 'n'", { expr = true, desc = 'Previous Search Result' })
 vim.keymap.set('x', 'n', "v:searchforward ? 'n' : 'N'", { expr = true, desc = 'Next Search Result' })
 vim.keymap.set('x', 'N', "v:searchforward ? 'N' : 'n'", { expr = true, desc = 'Previous Search Result' })
 vim.keymap.set('o', 'n', "v:searchforward ? 'n' : 'N'", { expr = true, desc = 'Next Search Result' })
 vim.keymap.set('o', 'N', "v:searchforward ? 'N' : 'n'", { expr = true, desc = 'Previous Search Result' })
-
--- Makes indenting in visual mode easier
-vim.keymap.set('v', '<', '<gv', { desc = 'Indent left' })
-vim.keymap.set('v', '>', '>gv', { desc = 'Indent right' })
 
 -- Some keymaps to fix behavior of "Home" and "End" keys while being in tmux window/pane
 vim.keymap.set({ 'n', 'v' }, '<Find>', '0')
